3ab2987fcdf38e2322e76bb76843ee4133e8252a
Ahmed Samy <ahmed.s.elsherbiny0@gmail.com>
2025-05-31 09:11:16 +0300

    edit java app

--- my-java-app/src/main/java/com/example/App.java
+++ my-java-app/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("samo 3aleekooooo");
+        System.out.println("hello from ahmed samy");
     }
 }
